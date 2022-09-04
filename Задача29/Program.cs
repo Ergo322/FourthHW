3593bb7c5480bb57b972d9862467accc3c2ae9a2
Ergo322 <Shikmen2@mail.ru>
2022-09-05 01:13:53 +0300

    task29

---
+++ Задача29/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] FillArray(int size)
+            {
+            int[]array = new int[size];
+            for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = new Random().Next(0, 30);
+                    if (i!=array.Length-1) Console.Write($"{array[i]}, ");
+                    else Console.WriteLine($"{array[i]}");
+                        
+                }
+            return array;
+            }    
+            int[] mas=FillArray(8);
