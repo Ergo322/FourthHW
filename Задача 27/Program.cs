68899fd6b1a506144be6ea6579e3f580c0705922
Ergo322 <Shikmen2@mail.ru>
2022-09-05 01:10:19 +0300

    task27

---
+++ Задача 27/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write ("Введите число: ");
+int A  = Convert.ToInt32 (Console.ReadLine());
+
+int SumA(int A)
+{
+    int massiv = Convert.ToString(A).Length;
+    int count = 0;
+    int result = 0;
+        for (int i = 0; i < massiv; i++)
+        {
+            count = A - A % 10;
+            result = result + (A - count);
+            A = A / 10;
+        }
+        return result;
+}
+int sumNumber = SumA(A);
+Console.WriteLine ($"Сумма цифр в числе: {sumNumber}" );
